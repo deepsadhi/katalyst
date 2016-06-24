2ee27403b13cc9d65dfb20988b5a0e5f362cd1ad
Deepak <deeps.adhi@gmail.com>
2016-06-25 03:06:21 +0545

    recrusion magic classification of nodes

--- magic.py
+++ magic.py
@@ -6,13 +6,25 @@ class Magic():
 	MULTIPLIER = 1
 	# TODO: calculate multiplier
 
-	# def __int__(self):
-	# 	Svg.__int__(self)
-
 	def unit_correction(self, number):
 		number = float(number)
 		return int(number / 4) * 4
 
+	def recursion_magic(self, element):
+		if element.tag == self.SVG_TEXT:
+			print(element.tag)
+		elif element.tag == self.SVG_G:
+			attribute = element.attrib['id'].lower()
+			if re.match('image', attribute):
+				print(attribute)
+			elif re.match('inputfield', attribute):
+				print(attribute)
+			elif re.match('button', attribute):
+				print(attribute)
+			else:
+				for node in element.getiterator():
+					if node.getparent() == element:
+						self.recursion_magic(node)
 
 	def magic(self):
 		art_boards = self.svg_layout_tree.xpath('//ns:svg/ns:g',
@@ -21,33 +33,13 @@ class Magic():
 		art_board_dimensions = []
 		for art_board in art_boards:
 			for element in art_board.getiterator():
-				if element.tag == self.SVG_RECT:
-					dimension = {'width': self.unit_correction(element.attrib['width']),
-								 'height': self.unit_correction(element.attrib['height'])}
-
-					if 'x' in element.attrib:
-						dimension['left'] = float(element.attrib['x'])
-					else:
-						dimension['left'] = 0
-
-					if 'y' in element.attrib:
-						dimension['top'] = float(element.attrib['y'])
-					else:
-						dimension['top'] = 0
-
-					dimension['right'] = dimension['left'] + dimension['width']
-					dimension['bottom'] = dimension['top'] + dimension['height']
-
-					art_board_dimensions.append(dimension)
-
 				if element.getparent() == art_board:
-					if element.tag == self.SVG_TEXT:
-						print(element.tag)
-					else:
+					if 'id' in element.attrib:
 						attribute = element.attrib['id'].lower()
-
-						if re.match('layout', attribute):
-							print(element.tag)
-
-
+						if element.tag == self.SVG_G and (re.match('statusbar', attribute) or re.match('actionbar', attribute)):
+							continue
+						else:
+							self.recursion_magic(element)
 			print('-')
+
+
